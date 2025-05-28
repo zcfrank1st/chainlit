894a04de39640fa7bd5872f83b56886ce0a26bdb
zcfrank1st <zhchaos@gmail.com>
2025-05-28 20:30:44 +0800

    http copy

--- frontend/src/components/CopyButton.tsx
+++ frontend/src/components/CopyButton.tsx
@@ -27,7 +27,30 @@ const CopyButton = ({ content, className }: Props) => {
           ? JSON.stringify(content, null, 2)
           : String(content);
 
-      await navigator.clipboard.writeText(textToCopy);
+      // navigator clipboard 需要https等安全上下文
+      if (navigator.clipboard && window.isSecureContext) {
+        // navigator clipboard 向剪贴板写文本
+        await navigator.clipboard.writeText(textToCopy);
+      } else {
+        // 创建text area
+        let textArea = document.createElement("textarea");
+        textArea.value = textToCopy;
+        // 使text area不在viewport，同时设置不可见
+        textArea.style.position = "absolute";
+        textArea.style.opacity = "0";
+        textArea.style.left = "-999999px";
+        textArea.style.top = "-999999px";
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+        
+        await new Promise((res, rej) => {
+          // 执行复制命令并移除文本框
+          document.execCommand('copy') ? res(undefined) : rej();
+          textArea.remove();
+        });
+      }
+
       setCopied(true);
 
       // Reset copied state after 2 seconds
